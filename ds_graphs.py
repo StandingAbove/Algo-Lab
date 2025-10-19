07866bbf4c7b23df92461908c6435c8b31d5f2fd
Sam(Chaoyue) Geng <SamGeng14@gmail.com>
2025-10-18 22:49:04 -0500

    new

--- ds_graphs.py
+++ ds_graphs.py
@@ -19,7 +19,42 @@ def dfs(node: GraphNode, visited: Set[GraphNode]):
     for neight in node.neighbors
         if neighbor not in visited
             dfs(neighbor, visited)
+def graph_deep_copy_dfs (node: GraphNode) -> GraphNode:
+    if not node:
+        return None
+    return dfs(node)
+def dfs(node: GraphNode, clone_map{}) -> GraphNode:
+    if node in clone_map:
+        return clone_map[node]
+    cloned_node = GraphNode(node.val)
+    clone_map[node] = cloned_node
+    for neighbor in node.neighbors:
+        cloned_neighbor = dfs(neighbor, clone_map)
+        cloned_node.neighbors.append(cloned_neighbor)
+    return cloned_node
 
+def count_islands(matrix: List[List[int]]) -> int:
+    if not matrix:
+        return 0
+    count = 0
+    for r in range(len(matrix)):
+        for c in range(len(matrix[0])):
+            if matrix[r][c] == 1:
+                dfs(r,c,matrix)
+                count += 1
+    return count
+def dfs(r: int, c: int, matrix: List[List[int]]) -> None:
+    matrix[r][c] = -1
+    dirs = [(-1, 0), (1, 0), (0, -1),(0, 1)]
+
+    for d in dirs:
+        next_r, next_c = r + d[0], c + d[1]
+        if(is_within_bounds(next_r, next_c, matrix)
+           and matrix[next_r][next_c] == 1):
+            dfs(next_r, next_c, matrix)
+
+def is_within_bounds(r: int, c: int, matrix: List[List[int]]) -> bool:
+    return 0 <= r < len(matrix) and 0 <= c < len(matrix[0])
 #BFS
 def bfs(node: GraphNode):
     visited = set()
